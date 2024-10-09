bbc5ab31c497d2d93a6c497adddc25a91ce3cd45
florence <loracodes3@gmail.com>
2024-10-09 18:42:06 +0300

    lexical scope you can access anything in the parent scope but the reverse is not true

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// Global scope --accessible in every scope, its the outermost scope
+// 1.Global scope --accessible in every scope, its the outermost scope
 let globalVar = "I am a global variable"; // Global variable
 
 function outerFunction() {
-    // Functional scope
+    // 2.Functional scope
     let outerVar = "I am an outer variable"; // Variable accessible only in outerFunction
 
     {
@@ -14,7 +14,7 @@ function outerFunction() {
     }
 
     function innerFunction() {
-        // Lexical scope
+        // Lexical scope  -- yopu can access anything in your parent scope i.e functional scope can access anything in the dlobal scope but the reverse is not true
         console.log(globalVar); // Accessible: Output: I am a global variable
         console.log(outerVar); // Accessible: Output: I am an outer variable
         // console.log(blockVar); // Un-commenting this line would cause an error: blockVar is not defined
